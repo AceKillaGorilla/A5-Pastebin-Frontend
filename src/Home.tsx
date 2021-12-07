65dee8a912743d703e190ae1f44f57cc643eafce
Jo <joely.rooke@gmail.com>
2021-12-07 14:26:34 +0000

    made Home and Latest pasties function components

---
+++ src/Home.tsx
@@ -0,0 +1,5 @@
+import LatestPasties from "./LatestPasties";
+
+export default function Home(): JSX.Element {
+  return <LatestPasties />;
+}
---
+++ src/LatestPasties.tsx
@@ -0,0 +1,5 @@
+export default function LatestPasties():JSX.Element {
+    return (
+        
+    )
+}
