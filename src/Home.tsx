f33cd3cb354a7d1a0b47b0f9f6a604ea48518ae3
Jo <joely.rooke@gmail.com>
2021-12-07 15:09:14 +0000

    passed props down to LatestPasties component:

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
 import LatestPasties from "./LatestPasties";
 
+export interface PastieProps {
+  id: number;
+  title: string;
+  contents: string;
+  created_timestamp: string;
+}
+
 export default function Home(): JSX.Element {
-  return <LatestPasties />;
+  const [pasties, setPasties] = useState<PastieProps[]>([]);
+
+  async function fetchPasties() {
+    const response = await fetch("https://c3a5-pastebin.herokuapp.com/latest");
+    const jsonBody: PastieProps[] = await response.json();
+    setPasties(jsonBody);
+  }
+
+  return (
+    <LatestPasties
+      key={id}
+      id={id}
+      title={title}
+      contents={contents}
+      created_timestamp={created_timestamp}
+    />
+  );
 }
--- src/LatestPasties.tsx
+++ src/LatestPasties.tsx
@@ -1,5 +1,17 @@
-export default function LatestPasties():JSX.Element {
-    return (
-        
-    )
+import { PastieProps } from "./Home";
+
+export default function LatestPasties({
+  title,
+  contents,
+  created_timestamp,
+} = PastieProps): JSX.Element {
+  return (
+    <>
+      <h6>{title}</h6>
+      <p>{contents}</p>
+      <p>{created_timestamp}</p>
+      <button>➕</button>
+      <button>➖</button>
+    </>
+  );
 }
